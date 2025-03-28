dd60acbc7765bf35daf9a720f2a654d9a9a21f41
Julia Montuori <juliamontuori@Julias-MacBook-Pro-182.local>
2025-03-28 11:38:35 -0400

    helper function for dot string comparision

--- test/dotast_test.ml
+++ test/dotast_test.ml
@@ -1,5 +1,21 @@
 open OUnit2
 
-let test_simple_dot _ = assert_equal true true (* put the dot ast test here *)
-let suite = "Dot Tests" >::: [ "test_simple_dot" >:: test_simple_dot ]
+let deq (pir :string) (dot_expected) =
+  let dot_from_program = Dot_ast.generate_dot_code pir in
+  let dot_actual = (Ast_utils.stringify_dot_choreo_ast dot_from_program) in
+  try 
+    assert_equal dot_actual dot_expected
+  with OUnit2.Assert_failure _ ->
+    Printf.printf "\n\nExpected DOT:\n%s\n\n" dot_expected;
+    Printf.printf "Actual DOT:\n%s\n\n" dot_actual;
+    (* Re-raise the exception after printing to still fail the test *)
+    raise (OUnit2.Assert_failure ("DOT strings are not equal", 0, 0))
+;;
+
+let test_1_dot _ = deq Dotgen_testcases.pir_1 Dotgen_testcases.dot_1
+;;(* put the dot ast test here *)
+
+let test_2_dot _ = deq Dotgen_testcases.pir_2 Dotgen_testcases.dot_2
+;;
+let suite = "Dot Tests" >::: [ "test_simple_dot" >:: test_1_dot ]
 let () = run_test_tt_main suite
--- test/dotgen_testcases.ml
+++ test/dotgen_testcases.ml
@@ -1,4 +1,3 @@
-
 let pir_1 =
   "main := let R.x := S.3 ~> R; in 
   if R.(x>5) 
@@ -8,44 +7,44 @@ let pir_1 =
        S.\"Bye\";"
 ;;
 
-let dot_1 =
-  digraph Example2 {
-    a[label=Assign]
-    b[label=main]
-    c[label=Let]
-    d[label=Assign]
-    e[label="R."]
-    f[label="Send: R."]
-    g[label=x]
-    h[label="S."]
-    i[label=3]
-    j[label=If]
-    k[label="R."]
-    l[label="Send: S."]
-    m[label="S."]
-    n[label="Send: S."]
-    o[label="S."]
-    p[label=BinOp]
-    q[label=x]
-    r[label=5]
-    s[label=">"]
-    t[label="R[L]"]
-    u[label="\"Hello\""]
-    v[label="R[R]"]
-    w[label="\"Bye\""]
-    
-    a -> {b c};
-    c -> {d j};
-    d -> {e f};
-    e -> g;
-    f -> h -> i;
-    j -> {k l m n o};
-    k -> p -> {q r s};
-    l -> t;
-    m -> u;
-    n -> v;
-    o -> w;
-}
+let dot_1 = 
+"digraph Example2 {
+  a[label=Assign]
+  b[label=main]
+  c[label=Let]
+  d[label=Assign]
+  e[label=\"R.\"]
+  f[label=\"Send: R.\"]
+  g[label=x]
+  h[label=\"S.\"]
+  i[label=3]
+  j[label=If]
+  k[label=\"R.\"]
+  l[label=\"Send: S.\"]
+  m[label=\"S.\"]
+  n[label=\"Send: S.\"]
+  o[label=\"S.\"]
+  p[label=BinOp]
+  q[label=x]
+  r[label=5]
+  s[label=\">\"]
+  t[label=\"R[L]\"]
+  u[label=\"\\\"Hello\\\"\"]
+  v[label=\"R[R]\"]
+  w[label=\"\\\"Bye\\\"\"]
+  
+  a -> {b c};
+  c -> {d j};
+  d -> {e f};
+  e -> g;
+  f -> h -> i;
+  j -> {k l m n o};
+  k -> p -> {q r s};
+  l -> t;
+  m -> u;
+  n -> v;
+  o -> w;
+}"
 ;;
 
 let pir_2 =
@@ -58,61 +57,61 @@ let pir_2 =
 ;;
 
 let dot_2 =
-  digraph Example2 {
-    a[label=Assign];
-    b[label=main];
-    c[label=If];
-    d[label="R."];
-    e[label=Let];
-    f[label=Let];
-    g[label=BinOp];
-    h[label=BinOp];
-    i[label=BinOp];
-    j[label=">"];
-    k[label=3];
-    l[label=5];
-    m[label="+"];
-    n[label=2];
-    o[label=1];
-    p[label="-"];
-    q[label=Assign];
-    r[label="R."];
-    s[label="R."];
-    t[label="Send: R"];
-    u[label=Res];
-    v[label=Pair]; //check
-    w[label=1];
-    x[label=true];
-    y[label="\"Why\""];
-    z[label=Assign];
-    aa[label="R."];
-    ab[label="R."];
-    ac[label="Send: R"];
-    ad[label=Res];
-    ae[label=Pair]; //check
-    af[label=0];
-    ag[label=false];
-    ah[label="\"Sent\""];
-    
-    a -> {b c};
-    c -> {d e f};
-    d -> g;
-    g -> {h i j};
-    h -> {k l m};
-    i -> {n o p};
-    e -> {q r};
-    q -> {s t};
-    s -> u;
-    t -> v;
-    v -> {w x};
-    r -> y;
-    f -> {z aa};
-    z -> {ab ac};
-    ab -> ad;
-    ac -> ae;
-    ae -> {af ag};
-    aa -> ah;
-}
+"digraph Example2 {
+  a[label=Assign];
+  b[label=main];
+  c[label=If];
+  d[label=\"R.\"];
+  e[label=Let];
+  f[label=Let];
+  g[label=BinOp];
+  h[label=BinOp];
+  i[label=BinOp];
+  j[label=\">\"];
+  k[label=3];
+  l[label=5];
+  m[label=\"+\"];
+  n[label=2];
+  o[label=1];
+  p[label=\"-\"];
+  q[label=Assign];
+  r[label=\"R.\"];
+  s[label=\"R.\"];
+  t[label=\"Send: R\"];
+  u[label=Res];
+  v[label=Pair]; //check
+  w[label=1];
+  x[label=true];
+  y[label=\"\\\"Why\\\"\"];
+  z[label=Assign];
+  aa[label=\"R.\"];
+  ab[label=\"R.\"];
+  ac[label=\"Send: R\"];
+  ad[label=Res];
+  ae[label=Pair]; //check
+  af[label=0];
+  ag[label=false];
+  ah[label=\"\\\"Sent\\\"\"];
+  
+  a -> {b c};
+  c -> {d e f};
+  d -> g;
+  g -> {h i j};
+  h -> {k l m};
+  i -> {n o p};
+  e -> {q r};
+  q -> {s t};
+  s -> u;
+  t -> v;
+  v -> {w x};
+  r -> y;
+  f -> {z aa};
+  z -> {ab ac};
+  ab -> ad;
+  ac -> ae;
+  ae -> {af ag};
+  aa -> ah;
+}"
 ;;
 
 let pir_3 =
@@ -126,40 +125,40 @@ let pir_3 =
 ;;
 
 let dot_3 =
-  digraph Example2 {
-    a[label=Assign];
-    b[label="y"];
-    c[label="P2."];
-    d[label=Assign];
-    e[label=y];
-    f[label=If];
-    g[label="P1."];
-    h[label="Send: P2."];
-    i[label="P2."];
-    j[label="Send: P2."];
-    k[label="P2."];
-    l[label=BinOp];
-    m[label=3];
-    n[label=5];
-    o[label=">"];
-    p[label="P1[L]"];
-    q[label=5];
-    r[label="P1[R]"];
-    s[label=9];
-    t[label=int];
-    
-    a -> {b c};
-    b -> d;
-    d -> {e f}; 
-    f -> {g h i j k};
-    g -> l;
-    l -> {m n o};
-    h -> p;
-    i -> q;
-    j -> r;
-    k -> s;
-    c -> t;
-}
+"digraph Example2 {
+  a[label=Assign];
+  b[label=\"y\"];
+  c[label=\"P2.\"];
+  d[label=Assign];
+  e[label=y];
+  f[label=If];
+  g[label=\"P1.\"];
+  h[label=\"Send: P2.\"];
+  i[label=\"P2.\"];
+  j[label=\"Send: P2.\"];
+  k[label=\"P2.\"];
+  l[label=BinOp];
+  m[label=3];
+  n[label=5];
+  o[label=\">\"];
+  p[label=\"P1[L]\"];
+  q[label=5];
+  r[label=\"P1[R]\"];
+  s[label=9];
+  t[label=int];
+  
+  a -> {b c};
+  b -> d;
+  d -> {e f}; 
+  f -> {g h i j k};
+  g -> l;
+  l -> {m n o};
+  h -> p;
+  i -> q;
+  j -> r;
+  k -> s;
+  c -> t;
+}"
 ;;
 
 
@@ -173,43 +172,43 @@ let pir_4 =
 ;;
 
 let dot_4 =
-  digraph Example4 {
-    a[label=Assign];
-    b[label=y];
-    c[label=Let];
-    d[label=Assign];
-    e[label=If];
-    g[label=P];
-    f[label=y];
-    h[label=y];
-    i[label=P];
-    j[label=P];
-    k[label=Let];
-    l[label=BinOp];
-    m[label=y];
-    n[label=3];
-    o[label=">"];
-    p[label=1];
-    q[label=Assign];
-    r[label=y];
-    s[label=P2];
-    t[label="Send: P2"];
-    u[label=a];
-    v[label=P1];
-    w[label=z]; 
-    
-    a -> {b c};
-    c -> {d e};
-    d -> {f g};
-    g -> h;
-    e -> {i j k};
-    i -> l;
-    l -> {m n o};
-    j -> p;
-    k -> {q r};
-    q -> {s t};
-    s -> u;
-    t -> v;
-    v -> w;
-}
+"digraph Example4 {
+  a[label=Assign];
+  b[label=y];
+  c[label=Let];
+  d[label=Assign];
+  e[label=If];
+  g[label=P];
+  f[label=y];
+  h[label=y];
+  i[label=P];
+  j[label=P];
+  k[label=Let];
+  l[label=BinOp];
+  m[label=y];
+  n[label=3];
+  o[label=\">\"];
+  p[label=1];
+  q[label=Assign];
+  r[label=y];
+  s[label=P2];
+  t[label=\"Send: P2\"];
+  u[label=a];
+  v[label=P1];
+  w[label=z]; 
+  
+  a -> {b c};
+  c -> {d e};
+  d -> {f g};
+  g -> h;
+  e -> {i j k};
+  i -> l;
+  l -> {m n o};
+  j -> p;
+  k -> {q r};
+  q -> {s t};
+  s -> u;
+  t -> v;
+  v -> w;
+}"
 ;;
