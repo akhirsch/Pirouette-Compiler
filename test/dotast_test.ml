19790a385b4d23fdc00b1fdfc2cea7665ebbeac3
Julia Montuori <juliamontuori@Julias-MacBook-Pro-182.local>
2025-03-28 20:11:43 -0400

    adding testcases and fixing dottest

--- test/dotast_test.ml
+++ test/dotast_test.ml
@@ -1,15 +1,11 @@
 open OUnit2
 
+let helper _ = ""
+;;
 let deq (pir :string) (dot_expected) =
-  let dot_from_program = Dot_ast.generate_dot_code pir in
-  let dot_actual = (Ast_utils.stringify_dot_choreo_ast dot_from_program) in
-  try 
-    assert_equal dot_actual dot_expected
-  with OUnit2.Assert_failure _ ->
-    Printf.printf "\n\nExpected DOT:\n%s\n\n" dot_expected;
-    Printf.printf "Actual DOT:\n%s\n\n" dot_actual;
-    (* Re-raise the exception after printing to still fail the test *)
-    raise (OUnit2.Assert_failure ("DOT strings are not equal", 0, 0))
+  let program = Parsing.Parse.parse_with_error (Lexing.from_string pir) in
+  let dot_actual = Ast_utils.stringify_dot_choreo_ast Parsing.Parsed_ast.string_of_pos program in
+  assert_equal dot_actual dot_expected
 ;;
 
 let test_1_dot _ = deq Dotgen_testcases.pir_1 Dotgen_testcases.dot_1
@@ -17,5 +13,20 @@ let test_1_dot _ = deq Dotgen_testcases.pir_1 Dotgen_testcases.dot_1
 
 let test_2_dot _ = deq Dotgen_testcases.pir_2 Dotgen_testcases.dot_2
 ;;
-let suite = "Dot Tests" >::: [ "test_simple_dot" >:: test_1_dot ]
+
+let test_3_dot _ = deq Dotgen_testcases.pir_3 Dotgen_testcases.dot_3
+;;
+
+let test_4_dot _ = deq Dotgen_testcases.pir_4 Dotgen_testcases.dot_4
+;;
+let suite =
+  "Dot Tests"
+  >::: [ "Examples"
+         >::: [ ("testcase1" >:: test_1_dot )
+              ; ("testcase2" >:: test_2_dot)
+              ; ("testcase3" >:: test_3_dot)
+              ; ("testcase4" >:: test_4_dot)
+              ]
+       ]
+;;
 let () = run_test_tt_main suite
