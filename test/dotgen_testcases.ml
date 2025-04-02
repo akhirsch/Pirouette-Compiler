243f75602ccf7dd206f3318ccb8be57a11976139
Julia Montuori <juliamontuori@Julias-MacBook-Pro-182.local>
2025-04-01 21:58:05 -0400

    added dotgen testcases

--- test/dotgen_testcases.ml
+++ test/dotgen_testcases.ml
@@ -123,8 +123,45 @@ let pir_3 =
     P2.9
 ;"
 ;;
-
-let dot_3 =
+  let dot_3 =
+    "digraph G {\n\
+    n0 [label=\"Decl [1:0-1:10]\"];\n\
+    n0 -> n1;\n\
+    n0 -> n2;\n\
+    n1 [label=\"y [1:0-1:1]\"];\n\
+    n2 [label=\"P2 [1:3-1:9]\"];\n\
+    n2 -> n3;\n\
+    n3 [label=\"Int [1:6-1:9]\"];\n\n\
+    n4 [label=\"Assign [2:2-7:1]\"];\n\
+    n4 -> n5;\n\
+    n4 -> n6;\n\
+    n5 [label=\"y [2:2-2:3]\"];\n\
+    n6 [label=\"If [2:7-6:8]\"];\n\
+    n6 -> n7;\n\
+    n6 -> n12;\n\
+    n6 -> n15;\n\
+    n7 [label=\"P1 [2:10-2:18]\"];\n\
+    n7 -> n8;\n\
+    n8 [label=\"BinOp [2:13-2:18]\"];\n\
+    n8 -> n9;\n\
+    n8 -> n10;\n\
+    n8 -> n11;\n\
+    n9 [label=\"3 [2:14-2:15]\"];\n\
+    n10 [label=\"5 [2:16-2:17]\"];\n\
+    n11 [label=\"> [2:15-2:16]\"];\n\
+    n12 [label=\"Sync: P1[L] -> P2 [3:7-4:8]\"];\n\
+    n12 -> n13;\n\
+    n13 [label=\"P2 [4:4-4:8]\"];\n\
+    n13 -> n14;\n\
+    n14 [label=\"5 [4:7-4:8]\"];\n\
+    n15 [label=\"Sync: P1[R] -> P2 [5:7-6:8]\"];\n\
+    n15 -> n16;\n\
+    n16 [label=\"P2 [6:4-6:8]\"];\n\
+    n16 -> n17;\n\
+    n17 [label=\"9 [6:7-6:8]\"];\n\n\
+    }"
+  
+let dot_3_example =
 "digraph Example2 {
   a[label=Assign];
   b[label=\"y\"];
