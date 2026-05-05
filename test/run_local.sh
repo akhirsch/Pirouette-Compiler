992f44a87b00dbc74179fe249767a029a67f4076
jackieparke <jparke1012@gmail.com>
2026-05-05 09:27:19 -0400

    added bash script for demo

---
+++ test/run_local.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+cd test/
+
+./demo_A.exe shortdemo.yaml &
+./demo_B.exe shortdemo.yaml &
+./demo_C.exe shortdemo.yaml &
+./demo_D.exe shortdemo.yaml &
+./demo_E.exe shortdemo.yaml &
+./demo_F.exe shortdemo.yaml &
+./demo_G.exe shortdemo.yaml &
+./demo_H.exe shortdemo.yaml &
+./demo_I.exe shortdemo.yaml &
+./demo_J.exe shortdemo.yaml &
+./demo_K.exe shortdemo.yaml &
+./demo_L.exe shortdemo.yaml &
+./demo_M.exe shortdemo.yaml &
+./demo_N.exe shortdemo.yaml &
+./demo_O.exe shortdemo.yaml &
+./demo_P.exe shortdemo.yaml &
+
+wait
